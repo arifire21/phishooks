ddff993d8f89210cf8017c96db51692ada9a600c
arifire21 <ariwolfpup@icloud.com>
2023-01-30 10:22:28 -0500

    basic input form setup

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -1,26 +1,33 @@
 import './App.scss';
 import Button from 'react-bootstrap/Button';
 import LightLogo from './logos/LogoLight.png';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={LightLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <Button variant='success'>TEST BTN</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={LightLogo} alt="logo" style={{marginBottom:"2rem"}}/>
       </header>
+
+        <Form>
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <Form.Group className="mb-3" controlId="formInput">
+              <Form.Label>URL or Hash</Form.Label>
+              <Form.Control type="text" />
+              </Form.Group>
+            </Col>
+            
+            <Col xs="auto" style={{marginTop:"16px"}}>
+              <Button variant="primary" type="submit">
+              Search
+              </Button>
+            </Col>
+          </Row>
+        </Form>
     </div>
   );
 }
