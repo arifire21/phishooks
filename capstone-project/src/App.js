c34d83f718f3d5e68bf8569bddad7e6ae65c8e69
arifire21 <ariwolfpup@icloud.com>
2023-02-12 23:43:35 -0500

    past res add row

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -22,18 +22,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={LightLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
 
         <Form>
--- capstone-project/src/components/PastResults.js
+++ capstone-project/src/components/PastResults.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-function PastResults() {
+function PastResults(newResult) {
+  //state variable for adding
+  //https://medium.com/codex/how-to-use-array-in-reactjs-2a30d8b72503
+  const [results, setResults] = useState([]);
+
+  const addRow=()=>{
+    let resForRow={name:newResult}
+    setResults([...results, resForRow])
+  }
+
   return (
     <ListGroup as="ol" numbered style={{float:"right"}}>
-      <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-      <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-      <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+      {/* <ListGroup.Item as="li">Cras justo odio</ListGroup.Item> */}
+
+      {results.map( (result, index)=>
+       (
+          <ListGroup.Item as="li" key={index}>
+              <h3>{result.name}</h3>
+              <p>test</p>
+          </ListGroup.Item>
+       )
+       )}
     </ListGroup>
   );
 }
