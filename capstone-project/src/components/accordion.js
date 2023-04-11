c6ef1ce109cc4f1797caec27eff29d3d2076d419
Edgar Jardines <ejardines101@gmail.com>
2023-04-11 09:00:14 -0400

    Started connecting the front and back ends

--- capstone-project/src/components/accordion.js
+++ capstone-project/src/components/accordion.js
@@ -1,6 +1,25 @@
 import Accordion from 'react-bootstrap/Accordion';
+import axios from 'axios';
+import React from 'react';
 
 function DetailsAccordion({header}) {
+
+  function scanUrl(url) {
+    axios.post('http://localhost:7006/api/scan-url', { url: url })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  React.useEffect(() => {
+    if (header) {
+      scanUrl(header);
+    }
+  }, [header]);
+
   return (
     <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
