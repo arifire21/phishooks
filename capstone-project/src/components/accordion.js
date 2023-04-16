e2bdda8e06e192ed2d498fe19f5409125c4d6105
Edgar Jardines <ejardines101@gmail.com>
2023-04-16 16:24:43 -0400

    Working on formating response

--- capstone-project/src/components/accordion.js
+++ capstone-project/src/components/accordion.js
@@ -1,25 +1,32 @@
 import Accordion from 'react-bootstrap/Accordion';
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 
 function DetailsAccordion({header}) {
+  const [result, setResult] = useState(null);
 
   function scanUrl() {
     axios.post('http://localhost:7006/api/scan-url', { url: header})
       .then((response) => {
-        return response.data;
+
+        setResult(response.data);
       })
       .catch((error) => {
-        return error;
+        setResult(error);
       });
   }
+
+  // Call the scanUrl function when the component mounts
+  React.useEffect(() => {
+    scanUrl();
+  }, []);
 
   return (
     <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
         <Accordion.Header>{header}</Accordion.Header>
         <Accordion.Body>
-        {JSON.stringify(scanUrl, null, 2)}
+          {result ? JSON.stringify(result, null, 2) : "Loading..."}/99 vendors view this URL as harmless.
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
@@ -27,3 +34,5 @@ function DetailsAccordion({header}) {
 }
 
 export default DetailsAccordion;
+
+
